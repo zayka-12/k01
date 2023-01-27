76ca45a9c22c5866bc897f9ecd5caad450c0f376
zayka <120934899+zayka-12@users.noreply.github.com>
2023-01-27 13:41:19 +0300

    comment

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать Math.Pow
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+ToDegree(numberA, numberB);
+
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
